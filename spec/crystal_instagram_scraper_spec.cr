a6f9607381a1d9107910fc89b001ebee07fb6ce2
Miguel Savignano <migue.masx@gmail.com>
2017-09-27 02:44:04 +0200

    test crystal all methods

--- spec/crystal_instagram_scraper_spec.cr
+++ spec/crystal_instagram_scraper_spec.cr
@@ -1,9 +1,35 @@
 require "./spec_helper"
 
 describe CrystalInstagramScraper do
-  it ".get_user_media_nodes" do
-    username = "the.cocktail"
-    response = CrystalInstagramScraper.get_user_media_nodes(username)
-    response.should be_a(JSON::Any)
+
+  it ".search" do
+    CrystalInstagramScraper.search("the.cocktail")
+    .should be_a(JSON::Any)
   end
+
+  it ".get_user_media_nodes" do
+    CrystalInstagramScraper.get_user_media_nodes("the.cocktail")
+    .should be_a(JSON::Any)
+  end
+
+
+  it ".get_user" do
+    CrystalInstagramScraper.get_user("the.cocktail")
+    .should be_a(JSON::Any)
+  end
+
+  # it ".get_tag_media_nodes" do
+  #   CrystalInstagramScraper.get_tag_media_nodes("the.cocktail")
+  #   .should be_a(JSON::Any)
+  # end
+
+  # it ".get_media" do
+  #   CrystalInstagramScraper.get_media("vKQeMNu7H1")
+  #   .should be_a(JSON::Any)
+  # end
+
+  # it ".get_media_comments" do
+  #   CrystalInstagramScraper.get_media_comments("6zVfmqAMkD")
+  #   .should be_a(JSON::Any)
+  # end
 end
